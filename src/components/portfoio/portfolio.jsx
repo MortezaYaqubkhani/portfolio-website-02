5ca8fc14b87794f18a0e431d8e00fb5f9ef4c4f2
MortezaYaqubkhani <yaqubkhani.m@gmail.com>
2021-08-02 12:53:43 +0200

    the portfoio is done perfectly

--- src/components/portfoio/portfolio.jsx
+++ src/components/portfoio/portfolio.jsx
@@ -1,9 +1,17 @@
 import PortfolioList from "../portfolioList/portfolioList";
 import "./portfolio.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  brandingPortfolio,
+} from "../portfolioData";
 
 const Portfolio = () => {
   const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
   const list = [
     { id: "featured", title: "Featured" },
     { id: "design", title: "Design" },
@@ -11,6 +19,28 @@ const Portfolio = () => {
     { id: "webApp", title: "Web App" },
     { id: "branding", title: "Branding" },
   ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+      case "webApp":
+        setData(webPortfolio);
+        break;
+      case "mobileApp":
+        setData(mobilePortfolio);
+        break;
+      case "design":
+        setData(designPortfolio);
+        break;
+      case "branding":
+        setData(brandingPortfolio);
+        break;
+      default:
+        setData(featuredPortfolio);
+    }
+  }, [selected]);
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
@@ -25,34 +55,15 @@ const Portfolio = () => {
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img
-            src="https://cdn.trendhunterstatic.com/thumbs/bea-banking-app.jpeg"
-            alt="banking app"
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://cdn.trendhunterstatic.com/thumbs/bea-banking-app.jpeg"
-            alt="banking app"
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://cdn.trendhunterstatic.com/thumbs/bea-banking-app.jpeg"
-            alt="banking app"
-          />
-          <h3>Banking App</h3>
-        </div>
-        <div className="item">
-          <img
-            src="https://cdn.trendhunterstatic.com/thumbs/bea-banking-app.jpeg"
-            alt="banking app"
-          />
-          <h3>Banking App</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item" id={d.id}>
+            <img
+              src={d.img}
+              alt={d.title}
+            />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
