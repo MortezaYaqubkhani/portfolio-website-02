674465838478d64309df8b403c091b62416f987f
MortezaYaqubkhani <yaqubkhani.m@gmail.com>
2021-08-02 23:54:08 +0200

    everything is ready we need to do rearrenging for different sizes

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,32 @@
-import "./contact.scss"
+import "./contact.scss";
+import { useState } from "react";
 
 const Contact = () => {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMessage] = useState(false)
+  ;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img
+          src="https://hr.cornell.edu/sites/default/files/pictures/dialog.png"
+          alt="contacts"
+        />
+      </div>
+      <div className="right">
+        <h2>Leave your message!</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks for your message!</span>}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
